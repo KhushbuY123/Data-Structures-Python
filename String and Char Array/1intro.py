a2d5023505bd5d4afcafa66819e7c9af2f2d5502
Khushbu Yadav <khushbuyad123@gmail.com>
2024-04-04 08:40:19 +0530

    Strings in Existence

---
+++ String and Char Array/1intro.py
@@ -0,0 +1,14 @@
+# arr=["a","b","c",""]
+# arr1=["Khushbu Yadav"]
+# arr[0]="Khushbu"
+# arr.append("Muskan")
+# print(arr[3])
+# print(arr)
+# print(arr1)
+arr=[]
+n=int(input("Enter the number of elements"))
+for i in range(n):
+    a=input()
+    arr.append(a)
+print(arr)
+list
